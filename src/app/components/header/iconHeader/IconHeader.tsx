3df6f2c601ceed937e40e6f87affbe18f0f88f97
Agustin Arleo <arleo.agustin20@gmail.com>
2024-09-01 00:35:23 -0300

    refactor font card

--- src/app/components/header/iconHeader/IconHeader.tsx
+++ src/app/components/header/iconHeader/IconHeader.tsx
@@ -1,19 +1,32 @@
-import style from './IconHeader.module.css'
-import logo from '../../../assets/iconH.png'
-function IconHeader(){
+import { useState, useEffect } from 'react';
+import style from './IconHeader.module.css';
+import logo from '../../../assets/iconH.png';
+import logoLigth from '../../../assets/iconLigth.png';
 
-    const scrollToTop = () => {
-        window.scrollTo({
-          top: -100,
-          behavior: 'smooth'
-        });
-      };
+function IconHeader() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-    return(
-        <>
-        <img onClick={scrollToTop} className={style.img} src={logo} />
-        </>
-    )
+  // Efecto para escuchar cambios en el tema
+  useEffect(() => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    setIsDarkMode(currentTheme === 'dark');
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: -100,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <img
+      onClick={scrollToTop}
+      className={style.img}
+      src={isDarkMode ? logo : logoLigth}
+      alt="Logo"
+    />
+  );
 }
 
 export default IconHeader;
